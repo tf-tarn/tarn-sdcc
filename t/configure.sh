c9859693347cdc88d8e4c957610673a6e4132217
tarn <tarn@elara>
2022-07-09 17:49:20 +0900

    t dir

---
+++ t/configure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+./configure --prefix=/home/tarn/projects/tarn_sdcc/install \
+            --disable-mcs51-port    \
+            --disable-z80-port      \
+            --disable-z180-port     \
+            --disable-r2k-port      \
+            --disable-r2ka-port     \
+            --disable-r3ka-port     \
+            --disable-sm83-port     \
+            --disable-tlcs90-port   \
+            --disable-ez80_z80-port \
+            --disable-z80n-port     \
+            --disable-ds390-port    \
+            --disable-ds400-port    \
+            --disable-pic14-port    \
+            --disable-pic16-port    \
+            --disable-hc08-port     \
+            --disable-s08-port      \
+            --disable-stm8-port     \
+            --disable-pdk13-port    \
+            --disable-pdk14-port    \
+            --disable-pdk15-port    \
+            --disable-mos6502-port

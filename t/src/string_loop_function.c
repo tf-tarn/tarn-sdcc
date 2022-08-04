7f67add699cd9412827e4338ac09da46b6a0dd55
tarn <tarn@elara>
2022-08-04 10:11:39 +0900

    Make test pass

--- t/src/string_loop_function.c
+++ t/src/string_loop_function.c
@@ -1,19 +1,25 @@
 __sfr __at(7) pic;
 
-void print(char *s) {
+void print(const char *s) {
     for (char i = 0; s[i]; ++i) {
         pic = s[i];
     }
     return;
 }
 
-char var = 1;
+char var;
 
 char main (char argc, char **argv) {
-    char *msg = "Hello, world!\n";
+    const char *msg = "Hello, world!\n";
     const char *msg_too_short = "Too short.\n";
     const char *msg_bad_command = "Huh!?\n";
     const char *msg_bad_number = "Bad number.\n";
+
+    if (argc) {
+        var = 1;
+    } else {
+        var = 1;
+    }
 
     switch (var) {
     case 0:

b0c2cb2a2621c791a8bcc33ce3de82f14513112e
tarn <tarn@elara>
2023-05-02 17:48:22 +0900

    Rename not-equal test to comparison

--- tarn-tests/support/comparison.asm
+++ tarn-tests/support/comparison.asm
@@ -2,7 +2,7 @@
 ; File Created by SDCC : free open source ANSI-C Compiler
 ; Version 4.2.0 #13081 (Linux)
 ;--------------------------------------------------------
-	.file	"not_equal.c"
+	.file	"comparison.c"
 	
 .include "/home/tarn/projects/tarnos/asm/src/macros/macros.s"
 ;--------------------------------------------------------
@@ -68,26 +68,26 @@ __sdcc_program_startup:
 ; code
 ;--------------------------------------------------------
 	.section code,"ax"
-;	src/not-equal.c: 3: int main(int argc, char **argv) {
+;	src/comparison.c: 3: int main(int argc, char **argv) {
 ;; genLabel
 ;	-----------------------------------------
 ;	 function main
 ;	-----------------------------------------
 	_main:
-;	src/not-equal.c: 4: volatile unsigned char a = 1;
+;	src/comparison.c: 4: volatile unsigned char a = 1;
 ;; genAssign
 	lad	_main_a_65536_2 + 0
 	mov	mem il ,1
-;	src/not-equal.c: 5: volatile unsigned int n = 1;
+;	src/comparison.c: 5: volatile unsigned int n = 1;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
 	lad	_main_n_65536_2 + 1
 	mov	mem il ,1
-;	src/not-equal.c: 7: pic = '1';
+;	src/comparison.c: 7: pic = '1';
 ;; genAssign
 	mov	pic il ,49
-;	src/not-equal.c: 8: while (a != 0) {
+;	src/comparison.c: 8: while (a != 0) {
 ;; genLabel
 L_main00101:
 ;; genIfx
@@ -97,10 +97,10 @@ L_main00101:
 	mov	alub zero
 	mov	test aluc
 	gotonz	L_main00103
-;	src/not-equal.c: 9: pic = '2';
+;	src/comparison.c: 9: pic = '2';
 ;; genAssign
 	mov	pic il ,50
-;	src/not-equal.c: 10: a = 0;
+;	src/comparison.c: 10: a = 0;
 ;; genAssign
 	lad	_main_a_65536_2 + 0
 	mov	mem il ,0
@@ -108,10 +108,10 @@ L_main00101:
 	goto	L_main00101
 ;; genLabel
 L_main00103:
-;	src/not-equal.c: 13: pic = '3';
+;	src/comparison.c: 13: pic = '3';
 ;; genAssign
 	mov	pic il ,51
-;	src/not-equal.c: 14: while (a == 0) {
+;	src/comparison.c: 14: while (a == 0) {
 ;; genLabel
 L_main00104:
 ;; genIfx
@@ -123,10 +123,10 @@ L_main00104:
 	gotonz	L_main00235
 	goto	L_main00106
 L_main00235:
-;	src/not-equal.c: 15: pic = '4';
+;	src/comparison.c: 15: pic = '4';
 ;; genAssign
 	mov	pic il ,52
-;	src/not-equal.c: 16: a = 1;
+;	src/comparison.c: 16: a = 1;
 ;; genAssign
 	lad	_main_a_65536_2 + 0
 	mov	mem il ,1
@@ -134,10 +134,10 @@ L_main00235:
 	goto	L_main00104
 ;; genLabel
 L_main00106:
-;	src/not-equal.c: 19: pic = '5';
+;	src/comparison.c: 19: pic = '5';
 ;; genAssign
 	mov	pic il ,53
-;	src/not-equal.c: 20: while (n != 0) {
+;	src/comparison.c: 20: while (n != 0) {
 ;; genLabel
 L_main00107:
 ;; genIfx
@@ -158,10 +158,10 @@ L_main00237:
 	mov	test aluc
 L_main00238:
 	gotonz	L_main00109
-;	src/not-equal.c: 21: pic = '6';
+;	src/comparison.c: 21: pic = '6';
 ;; genAssign
 	mov	pic il ,54
-;	src/not-equal.c: 22: n = 0;
+;	src/comparison.c: 22: n = 0;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
@@ -171,10 +171,10 @@ L_main00238:
 	goto	L_main00107
 ;; genLabel
 L_main00109:
-;	src/not-equal.c: 25: pic = '7';
+;	src/comparison.c: 25: pic = '7';
 ;; genAssign
 	mov	pic il ,55
-;	src/not-equal.c: 26: while (n == 0) {
+;	src/comparison.c: 26: while (n == 0) {
 ;; genLabel
 L_main00110:
 ;; genIfx
@@ -197,10 +197,10 @@ L_main00241:
 	gotonz	L_main00239
 	goto	L_main00112
 L_main00239:
-;	src/not-equal.c: 27: pic = '8';
+;	src/comparison.c: 27: pic = '8';
 ;; genAssign
 	mov	pic il ,56
-;	src/not-equal.c: 28: n = 1;
+;	src/comparison.c: 28: n = 1;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
@@ -210,10 +210,10 @@ L_main00239:
 	goto	L_main00110
 ;; genLabel
 L_main00112:
-;	src/not-equal.c: 30: pic = '9';
+;	src/comparison.c: 30: pic = '9';
 ;; genAssign
 	mov	pic il ,57
-;	src/not-equal.c: 32: while (n < 2) {
+;	src/comparison.c: 32: while (n < 2) {
 ;; genLabel
 L_main00113:
 ;; genCmp
@@ -236,10 +236,10 @@ L_main00244:
 	gotonz	L_main00242
 	goto	L_main00115
 L_main00242:
-;	src/not-equal.c: 33: pic = 0x40;
+;	src/comparison.c: 33: pic = 0x40;
 ;; genAssign
 	mov	pic il ,64
-;	src/not-equal.c: 34: ++n;
+;	src/comparison.c: 34: ++n;
 ;; genALUOp
 ;;	ALU plus (4)
 ;;	ALU operand size 2 2 1
@@ -257,10 +257,10 @@ L_main00242:
 	goto	L_main00113
 ;; genLabel
 L_main00115:
-;	src/not-equal.c: 37: pic = 0x41;
+;	src/comparison.c: 37: pic = 0x41;
 ;; genAssign
 	mov	pic il ,65
-;	src/not-equal.c: 38: while (n > 1) {
+;	src/comparison.c: 38: while (n > 1) {
 ;; genLabel
 L_main00116:
 ;; genCmp
@@ -283,10 +283,10 @@ L_main00247:
 	gotonz	L_main00245
 	goto	L_main00118
 L_main00245:
-;	src/not-equal.c: 39: pic = 0x42;
+;	src/comparison.c: 39: pic = 0x42;
 ;; genAssign
 	mov	pic il ,66
-;	src/not-equal.c: 40: --n;
+;	src/comparison.c: 40: --n;
 ;; genALUOp
 ;;	ALU minus (16)
 ;;	ALU operand size 2 2 1
@@ -304,14 +304,14 @@ L_main00245:
 	goto	L_main00116
 ;; genLabel
 L_main00118:
-;	src/not-equal.c: 45: a = 0;
+;	src/comparison.c: 45: a = 0;
 ;; genAssign
 	lad	_main_a_65536_2 + 0
 	mov	mem il ,0
-;	src/not-equal.c: 47: pic = 0x50;
+;	src/comparison.c: 47: pic = 0x50;
 ;; genAssign
 	mov	pic il ,80
-;	src/not-equal.c: 48: while (a != 0) {
+;	src/comparison.c: 48: while (a != 0) {
 ;; genLabel
 L_main00119:
 ;; genIfx
@@ -321,10 +321,10 @@ L_main00119:
 	mov	alub zero
 	mov	test aluc
 	gotonz	L_main00121
-;	src/not-equal.c: 49: pic = 0x51;
+;	src/comparison.c: 49: pic = 0x51;
 ;; genAssign
 	mov	pic il ,81
-;	src/not-equal.c: 50: a = 0;
+;	src/comparison.c: 50: a = 0;
 ;; genAssign
 	lad	_main_a_65536_2 + 0
 	mov	mem il ,0
@@ -332,14 +332,14 @@ L_main00119:
 	goto	L_main00119
 ;; genLabel
 L_main00121:
-;	src/not-equal.c: 53: a = 1;
+;	src/comparison.c: 53: a = 1;
 ;; genAssign
 	lad	_main_a_65536_2 + 0
 	mov	mem il ,1
-;	src/not-equal.c: 55: pic = 0x52;
+;	src/comparison.c: 55: pic = 0x52;
 ;; genAssign
 	mov	pic il ,82
-;	src/not-equal.c: 56: while (a == 0) {
+;	src/comparison.c: 56: while (a == 0) {
 ;; genLabel
 L_main00122:
 ;; genIfx
@@ -351,10 +351,10 @@ L_main00122:
 	gotonz	L_main00248
 	goto	L_main00124
 L_main00248:
-;	src/not-equal.c: 57: pic = 0x53;
+;	src/comparison.c: 57: pic = 0x53;
 ;; genAssign
 	mov	pic il ,83
-;	src/not-equal.c: 58: a = 1;
+;	src/comparison.c: 58: a = 1;
 ;; genAssign
 	lad	_main_a_65536_2 + 0
 	mov	mem il ,1
@@ -362,16 +362,16 @@ L_main00248:
 	goto	L_main00122
 ;; genLabel
 L_main00124:
-;	src/not-equal.c: 61: n = 0;
+;	src/comparison.c: 61: n = 0;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
 	lad	_main_n_65536_2 + 1
 	mov	mem il ,0
-;	src/not-equal.c: 63: pic = 0x54;
+;	src/comparison.c: 63: pic = 0x54;
 ;; genAssign
 	mov	pic il ,84
-;	src/not-equal.c: 64: while (n != 0) {
+;	src/comparison.c: 64: while (n != 0) {
 ;; genLabel
 L_main00125:
 ;; genIfx
@@ -392,10 +392,10 @@ L_main00250:
 	mov	test aluc
 L_main00251:
 	gotonz	L_main00127
-;	src/not-equal.c: 65: pic = 0x55;
+;	src/comparison.c: 65: pic = 0x55;
 ;; genAssign
 	mov	pic il ,85
-;	src/not-equal.c: 66: n = 0;
+;	src/comparison.c: 66: n = 0;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
@@ -405,16 +405,16 @@ L_main00251:
 	goto	L_main00125
 ;; genLabel
 L_main00127:
-;	src/not-equal.c: 69: n = 1;
+;	src/comparison.c: 69: n = 1;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
 	lad	_main_n_65536_2 + 1
 	mov	mem il ,1
-;	src/not-equal.c: 71: pic = 0x56;
+;	src/comparison.c: 71: pic = 0x56;
 ;; genAssign
 	mov	pic il ,86
-;	src/not-equal.c: 72: while (n == 0) {
+;	src/comparison.c: 72: while (n == 0) {
 ;; genLabel
 L_main00128:
 ;; genIfx
@@ -437,10 +437,10 @@ L_main00254:
 	gotonz	L_main00252
 	goto	L_main00130
 L_main00252:
-;	src/not-equal.c: 73: pic = 0x57;
+;	src/comparison.c: 73: pic = 0x57;
 ;; genAssign
 	mov	pic il ,87
-;	src/not-equal.c: 74: n = 1;
+;	src/comparison.c: 74: n = 1;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
@@ -450,16 +450,16 @@ L_main00252:
 	goto	L_main00128
 ;; genLabel
 L_main00130:
-;	src/not-equal.c: 77: n = 2;
+;	src/comparison.c: 77: n = 2;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
 	lad	_main_n_65536_2 + 1
 	mov	mem il ,2
-;	src/not-equal.c: 79: pic = 0x58;
+;	src/comparison.c: 79: pic = 0x58;
 ;; genAssign
 	mov	pic il ,88
-;	src/not-equal.c: 80: while (n < 2) {
+;	src/comparison.c: 80: while (n < 2) {
 ;; genLabel
 L_main00131:
 ;; genCmp
@@ -482,10 +482,10 @@ L_main00257:
 	gotonz	L_main00255
 	goto	L_main00133
 L_main00255:
-;	src/not-equal.c: 81: pic = 0x59;
+;	src/comparison.c: 81: pic = 0x59;
 ;; genAssign
 	mov	pic il ,89
-;	src/not-equal.c: 82: ++n;
+;	src/comparison.c: 82: ++n;
 ;; genALUOp
 ;;	ALU plus (4)
 ;;	ALU operand size 2 2 1
@@ -503,16 +503,16 @@ L_main00255:
 	goto	L_main00131
 ;; genLabel
 L_main00133:
-;	src/not-equal.c: 85: n = 1;
+;	src/comparison.c: 85: n = 1;
 ;; genAssign
 	lad	_main_n_65536_2 + 0
 	mov	mem il ,0
 	lad	_main_n_65536_2 + 1
 	mov	mem il ,1
-;	src/not-equal.c: 87: pic = 0x60;
+;	src/comparison.c: 87: pic = 0x60;
 ;; genAssign
 	mov	pic il ,96
-;	src/not-equal.c: 88: while (n > 1) {
+;	src/comparison.c: 88: while (n > 1) {
 ;; genLabel
 L_main00134:
 ;; genCmp
@@ -535,10 +535,10 @@ L_main00260:
 	gotonz	L_main00258
 	goto	L_main00136
 L_main00258:
-;	src/not-equal.c: 89: pic = 0x61;
+;	src/comparison.c: 89: pic = 0x61;
 ;; genAssign
 	mov	pic il ,97
-;	src/not-equal.c: 90: --n;
+;	src/comparison.c: 90: --n;
 ;; genALUOp
 ;;	ALU minus (16)
 ;;	ALU operand size 2 2 1
@@ -556,19 +556,19 @@ L_main00258:
 	goto	L_main00134
 ;; genLabel
 L_main00136:
-;	src/not-equal.c: 92: pic = 0x62;
+;	src/comparison.c: 92: pic = 0x62;
 ;; genAssign
 	mov	pic il ,98
-;	src/not-equal.c: 102: __endasm;
+;	src/comparison.c: 102: __endasm;
 	halt
-;	src/not-equal.c: 104: return 0;
+;	src/comparison.c: 104: return 0;
 	mov	jmpl stack
 	mov	jmph stack
 	mov	stack il ,0
 	mov	stack il ,0
 	jump
 ;; genLabel
-;	src/not-equal.c: 105: }
+;	src/comparison.c: 105: }
 ;; genEndFunction
 	mov	jmpl stack
 	mov	jmph stack
